7b499d35f4b80137e159d009d42725fdad4d6299
Mats Bos <mats.bos@immalle.be>
2017-11-17 11:40:18 +0100

    gelukt

--- datum/Program.cs
+++ datum/Program.cs
@@ -9,6 +9,17 @@ namespace datum
     {
         static void Main(string[] args)
         {
+            DateTime nieuwjaar = new DateTime(2017, 1, 1);
+
+            while(nieuwjaar.Year <= 2027)
+            {
+                Console.Write(nieuwjaar.Year + ":");
+                Console.WriteLine(nieuwjaar.DayOfWeek);
+                
+                nieuwjaar = nieuwjaar.AddYears(1);
+
+            }
+
         }
     }
 }
